fa1af7a9b4453f0a7aadd6b2b377973d6ed558c5
Peter <baggersio@icloud.com>
2017-02-07 11:51:34 +0100

    Read and Use BookDetail Parameters + mergeMap example

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
+import { ActivatedRoute } from '@angular/router';
+import { BookDataService } from '../shared/book-data.service';
+// import 'rxjs/add/operator/mergeMap';
 
 @Component({
   selector: 'book-detail',
@@ -10,9 +13,18 @@ export class BookDetailComponent implements OnInit {
 
   public book: Book;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private bookService: BookDataService) {
+  }
 
   ngOnInit() {
+    this.route.params.subscribe((params: {isbn: string}) => {
+      this.bookService.getBookByIsbn(params.isbn).subscribe(book => this.book = book);
+    });
+
+    /*
+     this.route.params.mergeMap((params: { isbn: string }) => this.bookService.getBookByIsbn(params.isbn))
+     .subscribe(book => this.book = book);
+     */
   }
 
 }
