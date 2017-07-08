b841c18ff64d66031099734a0fbc8939e089e81b
David Muellerchen <dave@webdave.de>
2017-07-08 13:52:31 +0200

    test directive

--- src/app/shared/tooltip.directive.spec.ts
+++ src/app/shared/tooltip.directive.spec.ts
@@ -1,11 +1,45 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
 import { TooltipDirective } from './tooltip.directive';
+import {Component, DebugElement} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
 
-describe('TooltipDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TooltipDirective();
-    expect(directive).toBeTruthy();
+@Component({
+  selector: 'mock-component',
+  template: '<div tooltip></div>',
+})
+class MockComponent {
+}
+
+describe('A component with TooltipDirective', () => {
+  let fixture: ComponentFixture<MockComponent>;
+  let element: DebugElement;
+  let sut: TooltipDirective;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [MockComponent, TooltipDirective],
+    });
+
+    fixture = TestBed.createComponent(MockComponent);
+    element = fixture.debugElement.query(By.directive(TooltipDirective));
+    sut = element.injector.get(TooltipDirective);
+  });
+
+  fdescribe('tooltip', () => {
+    it('should default to be hidden', () => {
+      expect(sut.tooltipElement.hidden).toBeTruthy();
+    });
+
+    it('should be shown if mouse enter host', () => {
+      sut.onMouseEnter();
+
+      expect(sut.tooltipElement.hidden).toBeFalsy();
+    });
+
+    it('should be hidden if mouse leave host', () => {
+      sut.onMouseLeave();
+      
+      expect(sut.tooltipElement.hidden).toBeTruthy();
+    });
   });
 });
